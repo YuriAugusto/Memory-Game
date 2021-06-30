25ebe5c294a068d97dade150b27e0ab2730c294f
YuriAugusto <yuri.komesu@uni9.edu.br>
2021-06-30 10:26:05 -0300

    Adição da lógica que identifica quando duas cartas iguais são viradas

--- js/script.js
+++ js/script.js
@@ -4,23 +4,31 @@ const cards = document.querySelectorAll(".memory-card");//todos os elementos que
 let hasFlippedCard = false;//variável que diz quando a carta é virada, se inicia como false e quando clicada se torna true
 let firstCard, secondCard;
 
-function flipCard(){
+function flipCard() {
     this.classList.toggle("flip");//(this) ao ser clicado o elemento recebe a class flip, caso clique novamente a classe é retirada através do toggle()
     //console.log(this);//aqui eu faço a impressão do elemento clicado
     // console.log("Eu cliquei aqui!");
 
-    if(!hasFlippedCard){//se hasFlippedCard for diferente de false, significa que o usuário clicou no elemento
+    if (!hasFlippedCard) {//se hasFlippedCard for diferente de false, significa que o usuário clicou no elemento
         //primeiro click
         hasFlippedCard = true;
         firstCard = this;//firstCard recebe o elemento que foi clicado
-    }else{
+    } else {
         //segundo click
         hasFlippedCard = false;
         secondCard = this;//secondCard recebe o elemento que foi clicado
 
         //do cards match? as cartas combinam?
-        if(firstCard.dataset.framework){
-
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {//se o valor do atributo data- for identico nas duas cartas clicadas o evento de click que aciona a function que "flipa" será removido, impedindo o usuário de virar estas cartas novamente
+            //it's a match!
+            firstCard.removeEventListener("click", flipCard);
+            secondCard.removeEventListener("click", flipCard);
+        } else {//se o valor do data-framework for diferente nesse bloco eu faço a remoção da classe flip, responsável por mostrar a carta virada
+            //not a match
+            setTimeout(() =>{
+                firstCard.classList.remove("flip");//aqui
+                secondCard.classList.remove("flip");
+            }, 1500);//2º arg representa o tempo que a carta permanecera virada para cima
         }
     }
 }
